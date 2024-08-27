6953b09fdc3275054cce39bd6e7a42250a7ea77a
Aenan Bashir <aenanrocks@gmail.com>
2024-08-27 21:43:02 +0100

    redo most of the stuff

--- dental_lab_project/urls.py
+++ dental_lab_project/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, reverse
-from django.http import HttpResponseRedirect  # Import HttpResponseRedirect
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('orders/', include('orders.urls')),  # Include the URLs from the orders app
-    path('', lambda request: HttpResponseRedirect(reverse('order_list', args=[1]))),  # Redirect to the orders list for clinic with ID 1
-
+    path('orders/', include('orders.urls')),  # Include the orders app URLs
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,57 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import Order, User
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Order, User  # Importing models to use in views
+from django.urls import reverse
 from .forms import OrderForm
+from django.contrib.auth.decorators import login_required
 
 
-# View for listing all orders for a specific clinic
+# View to display all orders for a specific clinic
 def order_list(request, clinic_id):
-    clinic = get_object_or_404(User, id=clinic_id, role='clinic')
-  # Get the clinic or return a 404 error
-    orders = Order.objects.filter(clinic=clinic)
-  # Get all orders associated with this clinic
+    clinic = get_object_or_404(User, pk=clinic_id)  # Fetch the clinic based on ID
+    orders = Order.objects.filter(clinic=clinic)  # Get all orders related to this clinic
     return render(request, 'orders/order_list.html', {'clinic': clinic, 'orders': orders})
-  # Render the order list
 
-
-def order_detail(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    return render(request, 'orders/order_detail.html', {'order': order})
-
-# View for creating a new order
+# View to handle the creation of a new order
+@login_required
 def create_order(request, clinic_id):
-    clinic = get_object_or_404(User, id=clinic_id, role='clinic')
+    clinic = get_object_or_404(User, pk=clinic_id)  # Fetch the clinic based on ID
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            order = form.save(commit=False)
-            order.clinic = clinic
-            order.save()
-            return redirect('order_list', clinic_id=clinic.id)
+            new_order = form.save(commit=False)
+            new_order.clinic = clinic
+            new_order.save()
+            return redirect(reverse('orders:order_list', args=[clinic_id]))  # Redirect to the order list
     else:
         form = OrderForm()
-    return render(request, 'orders/order_form.html', {'form': form, 'clinic': clinic})
 
-# View for updating an existing order
-def update_order(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    if request.method == 'POST':
-        form = OrderForm(request.POST, instance=order)
-        if form.is_valid():
-            form.save()
-            return redirect('order_detail', order_id=order.id)
-    else:
-        form = OrderForm(instance=order)
-    return render(request, 'orders/order_form.html', {'form': form, 'clinic': order.clinic, 'order': order})
-
-def lab_dashboard(request):
-    orders = Order.objects.all()
-    return render(request, 'orders/lab_dashboard.html', {'orders': orders})
-
-# View for deleting an order
-def delete_order(request, order_id):
-    order = get_object_or_404(Order, id=order_id)  # Get the order or return a 404 error
-    if request.method == 'POST':
-        order.delete()  # Delete the order from the database
-        return redirect('order_list', clinic_id=order.clinic.id)  # Redirect to the order list view
-    return render(request, 'orders/delete_order.html', {'order': order})  # Render a confirmation page before deletion
+    return render(request, 'orders/create_order.html', {'form': form, 'clinic': clinic})
